0923d32b215aad20b6dc4cfb3628ee29238e8808
施慶銓 <ceall8650@gmail.com>
2017-07-18 02:15:37 +0800

    [修正] eslint樣式修改為Javascript standard Style

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const path = require('path');
+const path = require('path')
 const config = {
-	/**
- 	* webpack 執行環境，即 webpack 載入檔案時相對路徑的根目錄環境
+  /**
+  * webpack 執行環境，即 webpack 載入檔案時相對路徑的根目錄環境
   * 預設（沒有設定時）為執行指令（webpack）所在的那個目錄
   *
   * 【其他】
@@ -15,41 +15,41 @@ const config = {
   * 預設 context 會分別為 root/ 和 root/build/
   */
 
-	context: path.join(__dirname, 'src'),
-	/**
+  context: path.join(__dirname, 'src'),
+  /**
    * Entry point
    * 因為設定了 context 所以不需要加上 src/ 了
    */
-	entry: './app.js',
-	/**
+  entry: './app.js',
+  /**
    * 輸出路徑與檔名
    */
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'build.js'
-	},
-	/**
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'build.js'
+  },
+  /**
    * loaders 對應使用規則
    *
    * Babel 從 6.13.0 之後提供額外的參數 loose 和 modules
-	 * loose: 提供 loose 編譯模式，該模式啟動下 Babel 會盡可能產生較精簡的 ES5 程式碼，預設 false 會盡可能產出接近 ES2015 規範的程式碼。
-	 * modules: 轉換 ES2015 module 的語法（import）為其它類型，預設為 true 轉換為 commonjs。
+   * loose: 提供 loose 編譯模式，該模式啟動下 Babel 會盡可能產生較精簡的 ES5 程式碼，預設 false 會盡可能產出接近 ES2015 規範的程式碼。
+   * modules: 轉換 ES2015 module 的語法（import）為其它類型，預設為 true 轉換為 commonjs。
    */
-	module: {
-		rules: [
-   		{
-   			test: /\.js$/,
-   			exclude: /node_modules/,
-   			loader: 'babel-loader',
-   			options: {
-   				presets: [
-   					/* Loose mode and No native modules(Tree Shaking) */
-   					['es2015', { modules: false, loose: false }]
-					]
-				}
-			}
-		]
-	}
-};
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            /* Loose mode and No native modules(Tree Shaking) */
+            ['es2015', { modules: false, loose: false }]
+          ]
+        }
+      }
+    ]
+  }
+}
 
-module.exports = config;
+module.exports = config
